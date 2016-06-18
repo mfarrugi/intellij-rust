2ce97a462fc7d9c1d83a040cd7cbb78fe49e8bf5
Marco Farrugia <Marco.Farrugia@twosigma.com>
2016-06-18 19:05:10 -0400

    Questionably deduplicate path highlighting.

--- src/main/kotlin/org/rust/ide/annotator/RustHighlightingAnnotator.kt
+++ src/main/kotlin/org/rust/ide/annotator/RustHighlightingAnnotator.kt
@@ -12,7 +12,11 @@ import org.rust.lang.core.psi.util.elementType
 
 // Highlighting logic here should be kept in sync with tags in RustColorSettingsPage
 class RustHighlightingAnnotator : Annotator {
-    override fun annotate(element: PsiElement, holder: AnnotationHolder) = element.accept(object : RustElementVisitor() {
+
+    override fun annotate(element: PsiElement, _holder: AnnotationHolder) {
+        element.accept(object : RustElementVisitor() {
+        var holder = wrap(_holder)
+
         override fun visitAttr(o: RustAttrElement) {
             holder.highlight(o, RustColor.ATTRIBUTE)
         }
@@ -49,31 +53,14 @@ class RustHighlightingAnnotator : Annotator {
         }
 
         override fun visitPath(path: RustPathElement) {
-            val ref = path.reference.resolve()
+            val ref = path.reference.resolve() ?: return
 
-            // This highlights the *path* dependent on the reference type.
-            // @TODO This logic shouldn't be duplicated.
-            // recursionGuard(ref) { it.accept(this) } // Not sure how to get this to work on the resolved reference.
-            when {
-                ref is RustPatBindingElement && ref.isMut -> {
-                    holder.highlight(path.identifier, RustColor.MUT_BINDING)
-                }
-                ref is RustEnumItemElement   -> holder.highlight(path.identifier, RustColor.ENUM)
-                ref is RustStructItemElement -> holder.highlight(path.identifier, RustColor.STRUCT)
-                ref is RustTraitItemElement  -> holder.highlight(path.identifier, RustColor.TRAIT)
-                //
-                ref is RustFnItemElement -> holder.highlight(path.identifier, RustColor.FUNCTION_DECLARATION)
-                ref is RustImplMethodMemberElement -> {
-                    val color = if (ref.isStatic) RustColor.STATIC_METHOD else RustColor.INSTANCE_METHOD
-                    holder.highlight(path.identifier, color)
-                }
-                ref is RustTraitMethodMemberElement -> {
-                    val color = if (ref.isStatic) RustColor.STATIC_METHOD else RustColor.INSTANCE_METHOD
-                    holder.highlight(path.identifier, color)
-                }
-                ref is RustModDeclItemElement ||
-                ref is RustModItemElement -> holder.highlight(path.identifier, RustColor.MODULE)
+            if (ref is RustPathElement) {
+                // Not sure how a path references itself, this causes overflow otherwise.
+                return
             }
+            // This highlights the *path* dependent on the reference type.
+            resolveColor(ref)?.let { holder.highlight(path.identifier, it) }
         }
 
         // @TODO Bind types and paths similarly
@@ -97,15 +84,46 @@ class RustHighlightingAnnotator : Annotator {
             val color = if (o.isStatic) RustColor.STATIC_METHOD else RustColor.INSTANCE_METHOD
             holder.highlight(o.identifier, color)
         }
-    })
 
-    private fun AnnotationHolder.highlight(element: PsiElement?, color: RustColor?) {
+            // Capture the color of the element w/o mutating anything.
+        private fun resolveColor(e: PsiElement): RustColor? {
+            var outColor: RustColor? = null
+            val prevHolder = holder
+            this.holder = object : Highlighter {
+                override fun highlight(element: PsiElement?, textAttributesKey: TextAttributesKey?) {
+                    outColor = null;
+                }
+
+                override fun highlight(element: PsiElement?, color: RustColor?) {
+                    assert(outColor == null, {"Not a leaf element, ambiguous color."})
+                    outColor = color;
+                }
+            }
+            e.accept(this)
+            this.holder = prevHolder
+            return outColor
+        }
+
+    })
+}
+
+interface Highlighter {
+    fun highlight(element: PsiElement?, textAttributesKey: TextAttributesKey?);
+
+    fun highlight(element: PsiElement?, color: RustColor?) {
         highlight(element, color?.textAttributesKey)
     }
+}
 
-    private fun AnnotationHolder.highlight(element: PsiElement?, textAttributesKey: TextAttributesKey?) {
-        if (element != null && textAttributesKey != null) {
-            createInfoAnnotation(element, null).textAttributes = textAttributesKey
+fun wrap(holder: AnnotationHolder): Highlighter {
+    return object: Highlighter{
+
+        override fun highlight(element: PsiElement?, textAttributesKey: TextAttributesKey?) {
+            if (element != null && textAttributesKey != null) {
+                holder.createInfoAnnotation(element, null).textAttributes = textAttributesKey
+            }
         }
+
     }
+}
 }
