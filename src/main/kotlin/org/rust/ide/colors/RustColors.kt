0f14768810026156c4e7621fe55491a87fa7e935
Marco Farrugia <Marco.Farrugia@twosigma.com>
2016-07-01 00:08:29 -0400

    Remove extraneous field, reformat.

--- src/main/kotlin/org/rust/ide/colors/RustColors.kt
+++ src/main/kotlin/org/rust/ide/colors/RustColors.kt
@@ -7,55 +7,56 @@ import com.intellij.openapi.editor.DefaultLanguageHighlighterColors as Default
 /**
  * See [RustColorSettingsPage] and [org.rust.ide.highlight.syntax.RustHighlighter]
  */
-enum class RustColor(val humanName: String, val externalName: String, fallback: TextAttributesKey) {
-    IDENTIFIER            ("Identifier", "org.rust.IDENTIFIER", Default.IDENTIFIER),
-    FUNCTION_DECLARATION  ("Function declaration", "org.rust.FUNCTION_DECLARATION", Default.FUNCTION_DECLARATION),
-    INSTANCE_METHOD       ("Instance method declaration", "org.rust.INSTANCE_METHOD", Default.INSTANCE_METHOD),
-    STATIC_METHOD         ("Static method declaration", "org.rust.STATIC_METHOD", Default.STATIC_METHOD),
+enum class RustColor(val humanName: String, val default: TextAttributesKey) {
+    IDENTIFIER            ("Identifier",                  Default.IDENTIFIER),
+    FUNCTION_DECLARATION  ("Function declaration",        Default.FUNCTION_DECLARATION),
+    INSTANCE_METHOD       ("Instance method declaration", Default.INSTANCE_METHOD),
+    STATIC_METHOD         ("Static method declaration",   Default.STATIC_METHOD),
 
-    LIFETIME              ("Lifetime", "org.rust.LIFETIME", Default.IDENTIFIER),
+    LIFETIME              ("Lifetime",                    Default.IDENTIFIER),
 
-    CHAR                  ("Char", "org.rust.CHAR", Default.STRING),
-    STRING                ("String", "org.rust.STRING", Default.STRING),
-    NUMBER                ("Number", "org.rust.NUMBER", Default.NUMBER),
+    CHAR                  ("Char",                        Default.STRING),
+    STRING                ("String",                      Default.STRING),
+    NUMBER                ("Number",                      Default.NUMBER),
 
-    CRATE                 ("Crate",          "org.rust.CRATE",          Default.IDENTIFIER),
-    STRUCT                ("Struct",         "org.rust.STRUCT",         Default.CLASS_NAME),
-    TRAIT                 ("Trait",          "org.rust.TRAIT",          Default.INTERFACE_NAME),
-    MODULE                ("Module",         "org.rust.MODULE",         Default.IDENTIFIER),
-    ENUM                  ("Enum",           "org.rust.ENUM",           Default.CLASS_NAME),
-    ENUM_VARIANT          ("Enum Variant",   "org.rust.ENUM_VARIANT",   Default.STATIC_FIELD),
+    CRATE                 ("Crate",                       Default.IDENTIFIER),
+    STRUCT                ("Struct",                      Default.CLASS_NAME),
+    TRAIT                 ("Trait",                       Default.INTERFACE_NAME),
+    MODULE                ("Module",                      Default.IDENTIFIER),
+    ENUM                  ("Enum",                        Default.CLASS_NAME),
+    ENUM_VARIANT          ("Enum Variant",                Default.STATIC_FIELD),
 
-    FIELD                 ("Field",          "org.rust.FIELD",          Default.INSTANCE_FIELD),
+    FIELD                 ("Field",                       Default.INSTANCE_FIELD),
 
-    KEYWORD               ("Keyword", "org.rust.KEYWORD", Default.KEYWORD),
+    KEYWORD               ("Keyword",                     Default.KEYWORD),
 
-    BLOCK_COMMENT         ("Block comment", "org.rust.BLOCK_COMMENT", Default.BLOCK_COMMENT),
-    EOL_COMMENT           ("Line comment", "org.rust.EOL_COMMENT", Default.LINE_COMMENT),
-    DOC_COMMENT           ("Documentation comment", "org.rust.DOC_COMMENT", Default.DOC_COMMENT),
+    BLOCK_COMMENT         ("Block comment",               Default.BLOCK_COMMENT),
+    EOL_COMMENT           ("Line comment",                Default.LINE_COMMENT),
+    DOC_COMMENT           ("Documentation comment",       Default.DOC_COMMENT),
 
-    PARENTHESIS           ("Parenthesis", "org.rust.PARENTHESIS", Default.PARENTHESES),
-    BRACKETS              ("Brackets", "org.rust.BRACKETS", Default.BRACKETS),
-    BRACES                ("Braces", "org.rust.BRACES", Default.BRACES),
+    PARENTHESIS           ("Parenthesis",                 Default.PARENTHESES),
+    BRACKETS              ("Brackets",                    Default.BRACKETS),
+    BRACES                ("Braces",                      Default.BRACES),
 
-    OPERATORS             ("Operator sign", "org.rust.OPERATORS", Default.OPERATION_SIGN),
+    OPERATORS             ("Operator sign",               Default.OPERATION_SIGN),
 
-    SEMICOLON             ("Semicolon", "org.rust.SEMICOLON", Default.SEMICOLON),
-    DOT                   ("Dot", "org.rust.DOT", Default.DOT),
-    COMMA                 ("Comma", "org.rust.COMMA", Default.COMMA),
+    SEMICOLON             ("Semicolon",                   Default.SEMICOLON),
+    DOT                   ("Dot",                         Default.DOT),
+    COMMA                 ("Comma",                       Default.COMMA),
 
-    ATTRIBUTE             ("Attribute", "org.rust.ATTRIBUTE", Default.METADATA),
+    ATTRIBUTE             ("Attribute",                   Default.METADATA),
 
-    MACRO                 ("Macro", "org.rust.MACRO", Default.IDENTIFIER),
+    MACRO                 ("Macro",                       Default.IDENTIFIER),
 
-    TYPE_PARAMETER        ("Type parameter", "org.rust.TYPE_PARAMETER", Default.IDENTIFIER),
+    TYPE_PARAMETER        ("Type parameter",              Default.IDENTIFIER),
 
-    MUT_BINDING           ("Mutable binding", "org.rust.MUT_BINDING", Default.IDENTIFIER),
+    MUT_BINDING           ("Mutable binding",             Default.IDENTIFIER),
 
-    VALID_STRING_ESCAPE   ("Valid escape sequence", "org.rust.VALID_STRING_ESCAPE", Default.VALID_STRING_ESCAPE),
-    INVALID_STRING_ESCAPE ("Invalid excape sequence", "org.rust.INVALID_STRING_ESCAPE", Default.INVALID_STRING_ESCAPE);
+    VALID_STRING_ESCAPE   ("Valid escape sequence",       Default.VALID_STRING_ESCAPE),
+    INVALID_STRING_ESCAPE ("Invalid escape sequence",     Default.INVALID_STRING_ESCAPE),
+    ;
 
-    val textAttributesKey = TextAttributesKey.createTextAttributesKey(externalName, fallback)
+    val textAttributesKey = TextAttributesKey.createTextAttributesKey("org.rust.${this.name}", default)
     val attributesDescriptor = AttributesDescriptor(humanName, textAttributesKey)
 }
 
